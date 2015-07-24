49bb2c0183e130a8715af78f8935374ec22d92f3
Abdullah Öğük <aoguk1102@gmail.com>
2015-07-24 17:58:16 +0300

    -

--- project/database.php
+++ project/database.php
@@ -1,7 +1,12 @@
 <?php 
-	$connection = mysqli_connect("localhost", "user", "password", "whoop" );
+	$db_host = 'localhost';
+	$db_name = 'whoop';
+	$db_user = 'user';
+	$db_pass = 'password';
+	$connection = mysqli_connect($db_host, $db_user,$db_pass, $db_name );
 	if(mysqli_connect_errno()){
 		echo "Failed to connect MYSQL".mysqli_connect_errno();
+		exit;
 	}
 
  ?>
